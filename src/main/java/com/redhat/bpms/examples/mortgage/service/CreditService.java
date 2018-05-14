a12dab34c390d327413936c7a838ad55833f9394
Duncan Doyle <Duncan.Doyle@gmail.com>
2018-05-14 15:16:01 +0200

    Added Java-based CreditScore service implementation.

---
+++ src/main/java/com/redhat/bpms/examples/mortgage/service/CreditService.java
@@ -0,0 +1,7 @@
+package com.redhat.bpms.examples.mortgage.service;
+
+public interface CreditService {
+
+	int getCreditScore(int ssn);
+	
+}
---
+++ src/main/java/com/redhat/bpms/examples/mortgage/service/SimpleCreditService.java
@@ -0,0 +1,13 @@
+package com.redhat.bpms.examples.mortgage.service;
+
+public class SimpleCreditService implements CreditService {
+
+	@Override
+	public int getCreditScore(int ssn) {
+		int lastDigit = ssn - 10 * ( ssn / 10 );
+		int score = 600 + ( lastDigit * 20 );
+		System.out.println( "For ssn " + ssn + ", will return credit score of " + score );
+		return score;
+	}
+
+}
